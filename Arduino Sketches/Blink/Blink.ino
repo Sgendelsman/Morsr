d71e6e8fbb07b297eb92f578b1bafc3fede6f242
William Linker <wlinker1@umbc.edu>
2017-10-07 17:17:45 -0400

    Workout the kinks

--- Arduino Sketches/Blink/Blink.ino
+++ Arduino Sketches/Blink/Blink.ino
@@ -1,6 +1,7 @@
 /*
   Blink
-
+  This is an update. Please work.
+  
   Turns an LED on for one second, then off for one second, repeatedly.
 
   Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
